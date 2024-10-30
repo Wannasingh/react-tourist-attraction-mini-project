e2e3f596e25c38735f83cdc6226b68b7aaa19c96
Wannasingh <sarankhtn@gmail.com>
2024-10-30 10:45:44 +0700

    feat: set the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return <div className="text-3xl font-bold underlinen">Test</div>;
 }
 
 export default App;
